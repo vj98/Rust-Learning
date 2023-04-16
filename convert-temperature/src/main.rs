3c415db3a4c34c87efbc6717b5251a26a6aca011
Vijay Bhati <bhativijay17@gmail.com>
2023-04-16 22:19:46 +0530

    convert temperature fahrenheit to celsius

--- convert-temperature/src/main.rs
+++ convert-temperature/src/main.rs
@@ -1,3 +1,16 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut fahrenheit = String::new();
+    let mut num: f32 = 0.0;
+
+    println!("Please enter value in fahrenheit");
+    io::stdin()
+    .read_line(&mut fahrenheit)
+    .expect("Failed to read fahrenheit");
+
+    num = fahrenheit.trim().parse().expect("Not a valid string");
+
+    let ans = (num - 32.0) / 1.8;
+
+    println!("Converted value from fahrenheit to celsius {:.2}", ans);
 }
